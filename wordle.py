d7cd0b3dae4b4634bb4a53596bdde09b8ab69031
pabloparejo <parejo.p@gmail.com>
2022-03-23 12:32:00 +0100

    Replace tabs for spaces

--- wordle.py
+++ wordle.py
@@ -13,25 +13,25 @@ rounds = 0;
 found = False;
 
 while not found and rounds <= 6:
-	word_to_print = ''
-	found_chars = 0
-	guess = input('Dime una palabra: ').upper()
-	for (index, char) in enumerate(guess):
-		if char == word[index]:
-			found_chars += 1
-			word_to_print += Back.GREEN + f' {char} '
-		elif char in word:
-			word_to_print += Back.YELLOW + f' {char} '
-		else:
-			word_to_print += Back.RED + f' {char} '
+    word_to_print = ''
+    found_chars = 0
+    guess = input('Dime una palabra: ').upper()
+    for (index, char) in enumerate(guess):
+        if char == word[index]:
+            found_chars += 1
+            word_to_print += Back.GREEN + f' {char} '
+        elif char in word:
+            word_to_print += Back.YELLOW + f' {char} '
+        else:
+            word_to_print += Back.RED + f' {char} '
 
-	print(word_to_print + Style.RESET_ALL)
+    print(word_to_print + Style.RESET_ALL)
 
-	if found_chars == len(word):
-		found = True
+    if found_chars == len(word):
+        found = True
 
 print()
 if found:
-	print(Fore.GREEN + '¡Enhorabuena!' + Style.RESET_ALL)
+    print(Fore.GREEN + '¡Enhorabuena!' + Style.RESET_ALL)
 else:
-	print(Back.RED + '¡Perdiste!' + Style.RESET_ALL)
+    print(Back.RED + '¡Perdiste!' + Style.RESET_ALL)
